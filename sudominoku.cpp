40d3d7182094efd6a65e9400ea4d95a95f1c5f44
anderojas <anderojas1@gmail.com>
2016-06-30 18:38:09 -0500

    interfaz cambiada

--- sudominoku.cpp
+++ sudominoku.cpp
@@ -22,7 +22,11 @@ Sudominoku::Sudominoku(QWidget *parent) :
             if (campo == NULL) {
 
                 campo = new QTableWidgetItem();
-                campo->setText("0");
+                campo->setTextAlignment(Qt::AlignCenter);
+                //campo->setBackgroundColor(QColor(240,240,240));
+                campo->setBackgroundColor(QColor(0,0,0));
+
+                //campo->setText("");
                 ui->tableroJuego->setItem(i, j, campo);
 
             }
@@ -101,6 +105,9 @@ void Sudominoku::cargarAmbiente() {
 
         QTableWidgetItem *campo = ui->tableroJuego->item(espaciosVacios->at(posicionVector)->getx(), espaciosVacios->at(posicionVector)->gety());
         campo->setText(QString::number(i));
+        campo->setFlags(Qt::ItemIsEditable);
+        campo->setTextColor(QColor(255,255,255));
+        campo->setBackgroundColor(QColor(127, 127, 127));
         espaciosVacios->remove(posicionVector);
 
     }
@@ -125,6 +132,66 @@ void Sudominoku::llenarJuego() {
 
     srand(time(NULL));
 
+    int arrayJuego [3][9] = {{0,0,0,0,0,0,0,0,0},
+                            {0,0,0,0,0,0,0,0,0},
+                            {0,0,0,0,0,0,0,0,0},
+                            {0,0,0,0,0,0,0,0,0},
+                            {0,0,0,0,0,0,0,0,0},
+                            {0,0,0,0,0,0,0,0,0},
+                            {0,0,0,0,0,0,0,0,0},
+                            {0,0,0,0,0,0,0,0,0},
+                            {0,0,0,0,0,0,0,0,0}}
+                            {{0,0,0,0,0,0,0,0,0},
+                            {0,0,0,0,0,0,0,0,0},
+                            {0,0,0,0,0,0,0,0,0},
+                            {0,0,0,0,0,0,0,0,0},
+                            {0,0,0,0,0,0,0,0,0},
+                            {0,0,0,0,0,0,0,0,0},
+                           {0,0,0,0,0,0,0,0,0},
+                           {0,0,0,0,0,0,0,0,0},
+                           {0,0,0,0,0,0,0,0,0},
+                           {0,0,0,0,0,0,0,0,0},
+                           {0,0,0,0,0,0,0,0,0},
+                           {0,0,0,0,0,0,0,0,0},
+                           {0,0,0,0,0,0,0,0,0},
+                           {0,0,0,0,0,0,0,0,0},
+                           {0,0,0,0,0,0,0,0,0},
+                           {0,0,0,0,0,0,0,0,0},
+                           {0,0,0,0,0,0,0,0,0},
+
+                           };
+
+    QVector<int> *fila1 = new QVector<int>();
+    QVector<int> *fila2 = new QVector<int>();
+    QVector<int> *fila3 = new QVector<int>();
+    QVector<int> *fila4 = new QVector<int>();
+    QVector<int> *fila5 = new QVector<int>();
+    QVector<int> *fila6 = new QVector<int>();
+    QVector<int> *fila7 = new QVector<int>();
+    QVector<int> *fila8 = new QVector<int>();
+    QVector<int> *fila9 = new QVector<int>();
+
+    QVector<int> *columna1 = new QVector<int>();
+    QVector<int> *columna2 = new QVector<int>();
+    QVector<int> *columna3 = new QVector<int>();
+    QVector<int> *columna4 = new QVector<int>();
+    QVector<int> *columna5 = new QVector<int>();
+    QVector<int> *columna6 = new QVector<int>();
+    QVector<int> *columna7 = new QVector<int>();
+    QVector<int> *columna8 = new QVector<int>();
+    QVector<int> *columna9 = new QVector<int>();
+
+    QVector<int> *cuadricula1 = new QVector<int>();
+    QVector<int> *cuadricula2 = new QVector<int>();
+    QVector<int> *cuadricula3 = new QVector<int>();
+    QVector<int> *cuadricula4 = new QVector<int>();
+    QVector<int> *cuadricula5 = new QVector<int>();
+    QVector<int> *cuadricula6 = new QVector<int>();
+    QVector<int> *cuadricula7 = new QVector<int>();
+    QVector<int> *cuadricula8 = new QVector<int>();
+    QVector<int> *cuadricula9 = new QVector<int>();
+
+
     for (int i=1; i<10;i++){
         int contador=0;
         while (contador<8 && !espaciosVacios->empty()){
@@ -133,6 +200,13 @@ void Sudominoku::llenarJuego() {
 
             if (espaciosVacios->size() > 1)
                 posicion = rand()%(espaciosVacios->size()-1);
+
+            int col, row;
+
+            row = espaciosVacios->at(posicion)->getx();
+            col = espaciosVacios->at(posicion)->gety();
+
+            if (i )
 
             ui->tableroJuego->item(espaciosVacios->at(posicion)->getx(),
                                                     espaciosVacios->at(posicion)->gety())->setText(QString::number(i));
